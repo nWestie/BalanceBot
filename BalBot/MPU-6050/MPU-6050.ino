fb500beb20ae259af03a44a783b58f28521c41ab
Noah-West <westnoahb@gmail.com>
2021-09-16 21:51:41 -0400

    got raw IMU data working

--- BalBot/MPU-6050/MPU-6050.ino
+++ BalBot/MPU-6050/MPU-6050.ino
@@ -6,14 +6,21 @@
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 void setup(){
+  Wire.setSDA(18);
+  Wire.setSCL(19);
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  Serial.write("Starting");
 }
+unsigned long i;
 void loop(){
+  i = millis()+10;
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -25,13 +32,12 @@ void loop(){
   GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  Serial.println(AcX);
-  Serial.print(" | AcY = "); Serial.println(AcY);
-  Serial.print(" | AcZ = "); Serial.println(AcZ);
-  Serial.print(" | Tmp = "); Serial.println(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-  Serial.print(" | GyX = "); Serial.println(GyX);
-  Serial.print(" | GyY = "); Serial.println(GyY);
-  Serial.print(" | GyZ = "); Serial.println(GyZ);
-  Serial.println();
-  delay(333);
+  Serial.print("AcX:"); Serial.print(AcX);
+  Serial.print(",AcY:"); Serial.print(AcY);
+  Serial.print(",AcZ:"); Serial.print(AcZ);
+  Serial.print(",Tmp:"); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(",GyX:"); Serial.print(GyX);
+  Serial.print(",GyY:"); Serial.print(GyY);
+  Serial.print(",GyZ:"); Serial.println(GyZ);//*/
+  while(i > millis());
 }
