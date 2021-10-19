998a530934f31a701ef463024a4781c43999e5d0
Noah-West <westnoahb@gmail.com>
2021-10-18 23:32:35 -0400

    10/18 home: testing onboard voltage monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include <Arduino.h>
 #include <Encoder.h>
 //#define ENCODER_OPTIMIZE_INTERRUPTS
-
 Encoder m2(7, 8);
+const float BATTMULT = (3.3*13)/(3*1024);
+
 void setup(){
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   pinMode(15, OUTPUT); //m1 dir
   pinMode(17, OUTPUT); //m1 pwm
   digitalWrite(13, HIGH);
+  pinMode(20, INPUT);
 }
 void loop(){
-  Serial.println(m2.read());
+  Serial.println(analogRead(20)*BATTMULT);
+  delay(100);
+  /*Serial.println(m2.read());
   digitalWrite(15, HIGH);
   digitalWrite(17, HIGH);
   delay(1000);
@@ -26,5 +30,5 @@ void loop(){
   digitalWrite(15, LOW);
   digitalWrite(17, LOW);
   Serial.println(m2.read());
-  delay(1000);
+  delay(1000);*/
 }
