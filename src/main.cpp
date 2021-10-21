18b51dccae191f78f24ea16b12610c4bfccca2ed
Noah-West <westnoahb@gmail.com>
2021-10-21 13:33:53 -0400

    10/21 class, worked on IMU hookup

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <Arduino.h>
 #include <Encoder.h>
-#include <MPU6050.h>
-//#define ENCODER_OPTIMIZE_INTERRUPTS
+
+//encoder defines
+#define ENCODER_OPTIMIZE_INTERRUPTS
+
+
 Encoder m2(7, 8);
 const float BATTMULT = (3.3*13)/(3*1024);
 
 void setup(){
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
   pinMode(15, OUTPUT); //m1 dir
   pinMode(17, OUTPUT); //m1 pwm
-  digitalWrite(13, HIGH);
-  pinMode(20, INPUT);
+  pinMode(20, INPUT); //Batt Monitering
+
 }
 void loop(){
   Serial.println(analogRead(20)*BATTMULT);
