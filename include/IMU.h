66f790e8fd9a5881ccb16d9026fcaf35e4dfc4bd
Noah-West <westnoahb@gmail.com>
2021-10-29 13:33:53 -0400

    10/29 ind study: working on getting headers working with IMU class

--- include/IMU.h
+++ include/IMU.h
@@ -1,2 +1,34 @@
+#ifndef IMU_H
+#define IMU_H
 
-#include "i2c_t3.h"
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#define I2CDEV_IMPLEMENTATION       I2CDEV_ARDUINO_WIRE
+
+
+void dmpDataReady();
+class IMU {
+    private: 
+        MPU6050 mpu;
+        bool blinkState = false;
+        // MPU control/status vars
+        bool dmpReady = false;  // set true if DMP init was successful
+        uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+        uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+        uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+        uint16_t fifoCount;     // count of all bytes currently in FIFO
+        uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+        // orientation/motion vars
+        Quaternion q;           // [w, x, y, z]         quaternion container
+        VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+        VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+        VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+        VectorFloat gravity;    // [x, y, z]            gravity vector
+        float euler[3];         // [psi, theta, phi]    Euler angle container
+        float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+    public:
+        int init();
+        bool update();
+};
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,9 @@
 
 Encoder m2(7, 8);
 const float BATTMULT = (3.3*13)/(3*1024);
-
+IMU imu;
 void setup(){
+  imu.init();
   Serial.begin(9600);
   pinMode(15, OUTPUT); //m1 dir
   pinMode(17, OUTPUT); //m1 pwm
@@ -17,21 +18,7 @@ void setup(){
 }
 void loop(){
   //Serial.println(analogRead(20)*BATTMULT);
+  while(imu.update());
   delay(100);
-  /*Serial.println(m2.read());
-  digitalWrite(15, HIGH);
-  digitalWrite(17, HIGH);
-  delay(1000);
-  digitalWrite(15, LOW);
-  digitalWrite(17, LOW);
-  Serial.println(m2.read());
-  delay(1000);
-  Serial.println(m2.read());
-  digitalWrite(15, LOW);
-  digitalWrite(17, HIGH);
-  delay(1000); 
-  digitalWrite(15, LOW);
-  digitalWrite(17, LOW);
-  Serial.println(m2.read());
-  delay(1000);//*/
+  
 }
