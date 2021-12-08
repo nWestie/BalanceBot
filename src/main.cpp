9d3d7cb3f85c443c1a1ff8d77407f8bdf24ff89b
Noah-West <westnoahb@gmail.com>
2021-12-08 13:33:55 -0500

    12/8 Motor Wrapper working

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,56 @@
-#define DEBUG
+//#define DEBUG
 #include "debug.h"
 #include "Encoder.h"
 #include "PID_v1.h"
 #include "IMU.h"
 IMU imu;
 class Motor{
+private:
+  byte dirPin, pwmPin;
 public:
   Encoder enc = Encoder(0,0);
-  Motor(uint8_t pwm, uint8_t dir, uint8_t e1, uint8_t e2) {
-    dirPin = dir; pwmPin = pwm;
+  Motor(byte pwm, byte dir, uint8_t e1, uint8_t e2) {
+    dirPin = dir; 
+    pwmPin = pwm;
     enc = Encoder(e1, e2);
   }
-  void drive(int8_t pwm, bool dir){
+  void drive(byte pwm, byte dir){
     digitalWrite(dirPin, dir);
     analogWrite(pwmPin, pwm);
+    return;
   }
-private:
-  int8_t dirPin, pwmPin;
 };
 float ypr[3];
+Motor lMotor(17, 15, 7, 8); //pwm, dir, enc1, enc2
+Motor rMotor(16, 14, 5, 6);
 void setup(){
+  for(byte i = 5; i < 9; i++) pinMode(i, INPUT);
+  for(byte i = 14; i < 18; i++) pinMode(i, OUTPUT);
+  
   IFD while (Serial.available() && Serial.read()); // empty buffer
   IFD while (!Serial.available());                 // wait for data
   IFD while (Serial.available() && Serial.read()); // empty buffer again
 
   imu.setup();
   Serial.println("IMU Setup Successful");
-  Motor lMotor(17, 15, 7, 8);
-  Motor rMotor(16, 14, 5, 6);
   Serial.println(lMotor.enc.read());
-
+  // while (Serial.available() && Serial.read());
+  // while (!Serial.available());                
+  // while (Serial.available() && Serial.read());
 }
+bool alt = false;
 void loop(){
-  while (Serial.available() && Serial.read());
-  while (!Serial.available());                
-  while (Serial.available() && Serial.read());
-  if(imu.update(ypr)){
-    Serial.print("ypr\t");
-    Serial.print(ypr[0] * 180/M_PI);
-    Serial.print("\t");
-    Serial.print(ypr[1] * 180/M_PI);
-    Serial.print("\t");
-    Serial.println(ypr[2] * 180/M_PI);
-  }
+  if(alt)lMotor.drive(255, 1);
+  else lMotor.drive(0,1);
+  Serial.println(alt);
+  delay(1000);
+  alt = !alt;
+  // if(imu.update(ypr)){
+  //   Serial.print("ypr\t");
+  //   Serial.print(ypr[0] * 180/M_PI);
+  //   Serial.print("\t");
+  //   Serial.print(ypr[1] * 180/M_PI);
+  //   Serial.print("\t");
+  //   Serial.println(ypr[2] * 180/M_PI);
+  // }
 }
