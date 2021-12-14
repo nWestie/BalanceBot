b137dc50f083bc16c43ed3ebf9d422463f03a32a
Noah-West <westnoahb@gmail.com>
2021-12-14 13:34:25 -0500

    12/14 successful comms with phone

--- src/main.cpp
+++ src/main.cpp
@@ -21,27 +21,27 @@ public:
   }
 };
 float ypr[3];
-Motor lMotor(17, 15, 7, 8); //pwm, dir, enc1, enc2
-Motor rMotor(16, 14, 5, 6);
+//Motor lMotor(17, 15, 7, 8); //pwm, dir, enc1, enc2
+//Motor rMotor(16, 14, 5, 6);
 void setup(){
   for(byte i = 5; i < 9; i++) pinMode(i, INPUT);
   for(byte i = 14; i < 18; i++) pinMode(i, OUTPUT);
   Serial.begin(9600);
+  Serial2.begin(9600);
   IFD while (Serial.available() && Serial.read()); // empty buffer
   IFD while (!Serial.available());                 // wait for data
   IFD while (Serial.available() && Serial.read()); // empty buffer again
 
   //imu.setup();
-  Serial.println("IMU Setup Successful");
-  
-  //Serial1.begin(9600);
+  Serial.println("IMU Setup Successful");  
 }
+
 void loop(){
   //send any software serial data back out on serial link
-  if (Serial.available()){
-   Serial1.write(Serial.read());
-   Serial.print("s");
-  }  
-  //send any serial data back out on software serial link
-  if (Serial1.available())Serial.write(Serial1.read());
+  if(Serial2.available()){
+    char inp[3];
+    Serial2.readBytes(inp, 4);
+    for(byte i = 0;i<4;i++)Serial.print(inp[i], 16);
+    Serial.println();
+  }
 }
