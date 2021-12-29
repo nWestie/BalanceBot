6e211d6f77d8bcaf07649417212e880dcff09f14
Noah-West <westnoahb@gmail.com>
2021-12-29 11:46:34 -0500

    Speed up serial busses

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 //#define DEBUG
 #include <Arduino.h>
 void setup(){
-  Serial.begin(9600);
-  Serial2.begin(9600);
+  Serial.begin(38400);
+  Serial2.begin(38400);
 }
 void loop(){
   if(Serial.available())Serial2.write(Serial.read());
   if(Serial2.available())Serial.write(Serial2.read());
 }
+//AT+BAUD6
